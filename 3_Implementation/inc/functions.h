1a4c1853f098f5e5f761714038ab469418823ef5
Preethu25 <80700297+Preethu25@users.noreply.github.com>
2021-04-16 08:56:19 +0530

    Update functions.h

--- 3_Implementation/inc/functions.h
+++ 3_Implementation/inc/functions.h
@@ -1,19 +1,50 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+# ifndef WIN32_LEAN_AND_MEAN
+#   define WIN32_LEAN_AND_MEAN
+# endif
+# ifndef VC_EXTRALEAN
+#   define VC_EXTRALEAN
+# endif
+# ifndef NOMINMAX
+#   define NOMINMAX
+# endif
+#include<stdio.h>
+#include <windows.h>
+#include <stdbool.h>
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
 
-typedef struct {
-  int day;
-  int month;
-  int year;
-  char note[255];
-} Note;
+struct Date{
+    int dd;
+    int mm;
+    int yy;
+};
+struct Date date;
 
-int isLeapYear( int year );        /* True if leap year */
-int leapYears( int year );         /* The number of leap year */
-int todayOf( int y, int m, int d); /* The number of days since the beginning of the year */
-long days( int y, int m, int d);   /* Total number of days */
-void calendar(int y, int m);       /* display calendar at m y */
-int getDayNumber(int d,int m,int y);
+struct Remainder{
+    int dd;
+    int mm;
+    char note[50];
+};
+struct Remainder R;
+
+
+
+void gotoxy (int x, int y);
+void SetColor(int ForgC);
+void ClearColor();
+void ClearConsoleToColors(int ForgC, int BackC);
+int check_leapYear(int year);
+void increase_month(int *mm,  int *yy);
+void decrease_month(int *mm,  int *yy);
+
+int getNumberOfDays(int month,int year);
 char *getName(int day);
+void print_date(int mm, int yy);
+int getDayNumber(int day,int mon,int year);
+char *getDay(int dd,int mm,int yy);
+int checkNote(int dd, int mm);
+void printMonth(int mon,int year,int x,int y);
+void AddNote();
+void showNote(int mm);
 
 #endif
